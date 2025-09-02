f7fc8feba4ad27a5490a953475d3acc72d56e588
shuhei <sgw.sgw.shu@gmail.com>
2025-09-02 16:26:14 +0900

    【fix】for server root, set htaccess

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,8 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+const images = import.meta.glob('../images/icon/*.{png,jpg,jpeg,svg,gif}', { eager: true });
+
+// 確認用（必要なければ削除）
+console.log(images);
