a3aeca3b380bda56e34fa1db66726c37215d5379
shuhei <sgw.sgw.shu@gmail.com>
2025-10-18 19:50:02 +0900

    【fix】update test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,14 +6,14 @@
             <a href="{{ route('test') }}"
                 class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
                 <p class="text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-                    テストページ
+                    アドミンテストページ
                 </p>
             </a>
 
             <a href="{{ route('profile.edit') }}"
                 class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
                 <p class="text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-                    プロフィールページ
+                    ユーザーテストページ
                 </p>
             </a>
 
